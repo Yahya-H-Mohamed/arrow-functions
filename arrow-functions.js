d35380dbca42d6bb4724c30dc9307384cd3793cf
Yahya Mohamed <yahya.hm750@gmail.com>
2024-11-08 17:18:00 +0000

    add all forms of writing a function

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,34 @@
  */
 
 // Vanilla JavaScript Function
+/*function addTwoNumbers(a, b){
+    return a + b
+}
+const sum = addTwoNumbers(3, 5)
+console.log(sum)*/
 
 // Arrow Function With Parameters
+/*const addTwoNumbers = (a,b) => {
+    return a + b
+}
+let sum = addTwoNumbers(3, 5)
+console.log(sum)*/
 
 // Single Line Arrow Function With Parameters
+/*const addTwoNumbers = (a,b) => a + b
+let sum = addTwoNumbers(3, 5)
+console.log(sum)*/
 
 // Implicit Returns
+const saySomething = message => console.log(message)
+saySomething("Hello!")
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiline string so parenthesis
+        must be used to contain it
+     </p>`
+)
+
+console.log(returnMultipleLines())
